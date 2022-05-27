e99822992204b10d0df81ff3835c76e35ffc5b45
Cat Hanbit <changwoon4@naver.com>
2022-05-27 17:33:01 +0900

    save

---
+++ ES6 문법/ES6 자바스크립트 최신문법.js
@@ -0,0 +1,81 @@
+// 객체 초기화
+// let name = "noona";
+// let age = 17;
+// let cute = true;
+
+// let person = { name, age, cute };
+
+// console.log(person);
+
+// let person = {
+//   name: "noona",
+//   age: 17,
+//   cute: true,
+// };
+// let { name, age, cute } = person;
+
+// console.log(person);
+
+// let array = [1, 2, 3];
+// let [a, b, c] = array;
+
+// console.log(array);
+
+// let person = {
+//   name: "noona",
+//   age: 17,
+//   cute: true,
+// };
+
+// let { name, ...rest } = person;
+// console.log(rest);
+
+// let array = [1, 2, 3];
+// let [a, ...rest] = array;
+// console.log(rest);
+
+// let a = [1, 2];
+// let b = [3, 4];
+// let c = [5, 6];
+// let result = [...a, ...b, ...c];
+
+// console.log(result);
+
+// let name = "noona";
+// console.log("제 이름은 ${name}입니다");
+
+// let koo = (a, b) => {
+//   let result = a * b;
+//   return result;
+// };
+
+// console.log(koo);
+
+// const person = {
+//   points: 23,
+//   score: function () {
+//     this.points++;
+//   },
+// };
+
+// console.log(person);
+
+// class Car {
+//   constructor(make, color) {
+//     this.make = make;
+//     this.color = color;
+//   }
+// }
+// let hyundai = new Car("noona", "white");
+// Car.prototype.summary = function () {
+//   console.log(`This car is a ${this.make} in the colour ${this.colour}`);
+// }; // 여기서 화살표함수를 쓰면 안됀다  hyundai.summary()
+
+// let age = 20;
+// var obj = {
+//   age: 12,
+//   foo: function () {
+//     console.log(this.age);
+//   },
+// };
+// obj.foo();
