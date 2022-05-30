95adafc9d95c7df436c477a9d9a5290b923c537a
Cat Hanbit <changwoon4@naver.com>
2022-05-30 17:31:32 +0900

    save

--- 숫자 맞추기 게임/index.js
+++ 숫자 맞추기 게임/index.js
@@ -3,7 +3,7 @@
 //만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
 //랜덤번호가 < 유저번호 Down!!!
 //랜덤번호가 < 유저번호 Up!!
-//Rest버튼을 누르면 게임이 리셋이된다
+//Reset버튼을 누르면 게임이 리셋이된다
 //5번의 기회를 다쓰면 게임이 끝난다(더이상 추측 불가, 버튼이 disable)
 //유저가 1~100 범위 밖에 숫자를 입력하면 알려준다 기회를 깍지 않는다
 //유저가 이미 입력한 숫자를 또 입력하면 알려준다 기회를 깍지 않는다
@@ -64,7 +64,11 @@ function play() {
 function reset() {
   userInput.value = "";
   pickRandomNum();
-
-  resultArea.textContent = "결과값출력";
+  resultArea.textContent = "죽기 싫다면 맞춰라";
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList = [];
 }
 pickRandomNum();
