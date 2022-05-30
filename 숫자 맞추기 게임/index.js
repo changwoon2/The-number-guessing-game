e0f571dddccb6e3bd1ada64c10067259a7996491
Cat Hanbit <changwoon4@naver.com>
2022-05-30 16:25:56 +0900

    save

--- 숫자 맞추기 게임/index.js
+++ 숫자 맞추기 게임/index.js
@@ -16,9 +16,13 @@ let resetButton = document.getElementById("reset-button");
 let chances = 5;
 let gameOver = false;
 let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -29,6 +33,11 @@ function play() {
   let userValue = userInput.value;
   if (userValue < 1 || userValue > 100) {
     resultArea.textContent = "1과 100사이 숫자를 입력해 주세요";
+    return;
+  }
+  if (history.includes(userValue)) {
+    resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해주세요";
+    return;
   }
   chances--;
   chanceArea.textContent = `남은기회:${chances}번`;
@@ -40,7 +49,11 @@ function play() {
     resultArea.textContent = "down";
   } else {
     resultArea.textContent = "정답";
+    gameOver = true;
   }
+
+  history.push(userValue);
+
   if (chances < 1) {
     gameOver = true;
   }
